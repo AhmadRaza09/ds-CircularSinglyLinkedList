e1d0eaf329328d8f7cbc284f3c234b6cd1822a67
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-03-18 12:34:59 +0500

    add demo class for testing list. closes #2.

---
+++ CircularSinglyLinkedListDemo.java
@@ -0,0 +1,14 @@
+/**
+	Created By: Ahmad Raza
+	Date: 18 Mar 2022
+
+**/
+
+public class CircularSinglyLinkedListDemo
+{
+	
+	public static void main(String [] args)
+	{
+		CircularSinglyLinkedList l1 = new CircularSinglyLinkedList();
+	}
+}
